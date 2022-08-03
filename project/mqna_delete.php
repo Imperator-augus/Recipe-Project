255a8be1961202111d3debffb624cbd29b8ae859
CHAN YE <qjdrptmzk@naver.com>
2022-08-03 16:09:03 +0900

    오류수정중

--- project/mqna_delete.php
+++ project/mqna_delete.php
@@ -2,11 +2,14 @@
     include_once "db/db_board.php";
 
     $qust_no = $_GET["qust_no"];
+    $sql1 = "DELETE FROM a_board 
+    WHERE qust_no=$qust_no";
     $sql=
     "DELETE FROM q_board 
-    WHERE qust_no='$qust_no'";
+    WHERE qust_no=$qust_no";
 
     $conn=get_conn();
+    mysqli_query($conn,$sql1);
     $result=mysqli_query($conn,$sql);
     mysqli_close($conn);
      header("location: qna.php");
